9dc0d1848c0f16fc2ccde2e06fb9168e9bde92b9
bigujun <andersonj@hotmail.rs>
2018-04-18 23:42:26 -0300

    Change Log file location

--- run-fan.py
+++ run-fan.py
@@ -95,7 +95,7 @@ import datetime
 #########################
 sleepTime = 30	# Time to sleep between checking the temperature
                 # want to write unbuffered to file
-fileLog = open('/home/osmc/run-fan.log', 'w+', 0)
+fileLog = open('/home/run-fan.log', 'w+', 0)
 
 #########################
 # Log messages should be time stamped
